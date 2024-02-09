c03fc3152bf437b25b4c69d6d413f7ce31504cd2
Vishal01Dev <vishal01dev@gmail.com>
2024-02-09 12:03:40 +0530

    update package

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import ActiveStatus from "./components/sidebar/ActiveStatus";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ["latin"] });
 
